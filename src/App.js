5f43efa97e5eaf69e12a0838f8b8cd88264ca3cd
Henry <ark1159@daum.net>
2022-10-24 09:43:09 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import TextField from '@mui/material/TextField';
+import Button from '@mui/material/Button';
+
+
+const TodoItemInputField = (props) => {
+  const [input, setInput] = useState("");
+
+  const onSubmit = () => {
+    props.onSubmit(input);
+    setInput("");
+  };
+
+
+
+
+  return (<div>
+    <TextField
+       id="todo-item-input"
+       label="Todo Item"
+       variant="outlined"
+       onChange={(e) => setInput(e.target.value)} value={input}
+    />
+    <Button variant="outlined" onClick={onSubmit}>Submit</Button>
+   </div>);
+  };
+
+  const TodoItemList = (props) => {
+    const todoList = props.todoItemList.map((todoItem, index) => {
+      return <li key={index}>{todoItem.todoItemContent}</li>;
+    });
+
+
+
+    return (<div>
+      <ul>{todoList}</ul>
+    </div>;
+  };
+
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Hello World!
-        </a>
-      </header>
+      <TodoItemInputField onSubmit={() => {}} />
+      <TodoItemList todoItemList={[]} />
     </div>
     
   );
